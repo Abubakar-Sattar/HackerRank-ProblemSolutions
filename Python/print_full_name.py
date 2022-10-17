6aae1802da078c23fdb80d9f3f4b68a90a868cfc
Muhammad Shakeel <69622246+shakeel0581@users.noreply.github.com>
2022-10-17 07:08:23 +0500

    Create print_full_name

---
+++ Python/print_full_name.py
@@ -0,0 +1,15 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
