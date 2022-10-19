ad250512b80785b291cfe011ca60351f2804bcde
Huzaifa Nizamani <107719413+HuzaifaNizamani@users.noreply.github.com>
2022-10-19 05:39:24 +0500

    Create WelcomeToJava.java

---
+++ Java/WelcomeToJava.java
@@ -0,0 +1,15 @@
+// Name: Huzaifa Nizamani
+// Date: 19/10/2022
+
+import java.io.*;
+import java.util.*;
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World.");
+        
+        System.out.println("Hello, Java.");
+    }
+}
