ed7ace250bd6153e228196b96eb074099e8cf35a
ilyas123ahmed <80588277+ilyas123ahmed@users.noreply.github.com>
2022-10-20 22:24:23 +0500

    Create 3_ListComprehensions.py

---
+++ Python/3_ListComprehensions.py
@@ -0,0 +1,9 @@
+# Name: ILYAS AHMED
+# Date: 20/10/2022
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n])
