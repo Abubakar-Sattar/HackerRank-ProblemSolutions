1042c10f4265fd1a36a7981d2c70b833c82db91a
Abubakar Sattar <90573878+AbubakarSattar-dev@users.noreply.github.com>
2022-10-21 17:42:05 +0500

    Revert "Update JavaIf-Else.java"

--- Java/JavaIf-Else.java
+++ Java/JavaIf-Else.java
@@ -13,7 +13,7 @@ public class Solution {
 
 
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
