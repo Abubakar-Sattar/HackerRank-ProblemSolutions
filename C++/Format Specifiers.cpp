79c551bc1959e7eab907b71bf56e45e86f85cbcd
Hashir Bin masood <80586637+Hashir313@users.noreply.github.com>
2022-10-22 15:25:03 +0500

    Create Format Specifiers.cpp

---
+++ C++/Format Specifiers.cpp
@@ -0,0 +1,22 @@
+# Name: Hashir Bin Masood
+# Date: 22/10/2022
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char ch;
+    float fl;
+    double d;
+    cin>>i>>l>>ch>>fl>>d;
+    printf("%d\n",i);
+    printf("%ld\n",l);
+    printf("%c\n",ch);
+    printf("%.3f\n",fl);
+    printf("%.9lf\n",d);
+    return 0;
+}
