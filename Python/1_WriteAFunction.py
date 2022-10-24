6778dc2f7dceb50147208e5a52479ce832c6c0c1
Akshat Bokdia <101403537+AkshatBokdia0602@users.noreply.github.com>
2022-10-24 15:04:06 +0530

    Update 1_WriteAFunction.py

--- Python/1_WriteAFunction.py
+++ Python/1_WriteAFunction.py
@@ -1,18 +1,18 @@
-# Name : ILYAS AHMED
-# Date : 20/10/2022
+# Name : Akshat Bokdia
+# Date : 24/10/2022
 
 def is_leap(year):
     leap = False
-
-    # Write your logic here
-    if year % 400 == 0 :
-        leap = True
-    elif year % 100 == 0:
-       leap = False 
-    elif year % 4 == 0 :
-        leap = True  
     
+    if ((year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)):
+        leap = True
     return leap
 
-year = int(input())
-print(is_leap(year))
+if __name__ == "__main__":    
+    while True:
+        year = int(input("Enter a year: "))
+        if year < 0:
+            print("Invalid input!!")
+        else:
+            break
+    print(is_leap(year))
