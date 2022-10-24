4f873c061e011ab72f230f4c285110ed54b8f428
Zubair Naeem <zubairnaeem031@gmail.com>
2022-10-24 12:09:21 +0500

    C++ code Queue with array

---
+++ C++/ImplementQueueWithArray.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Array Implementation of Queue ----------------------------------------------------------------
+
+class Queue {
+public:
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
+
+Queue* createQueue(unsigned capacity)
+{
+    Queue* queue = new Queue();
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = capacity - 1; // This is important, see the enqueue
+    queue->array = new int[(queue->capacity * sizeof(int))];
+    return queue;
+}
+
+int isFull(Queue* queue)
+{
+    return (queue->size == queue->capacity);
+}
+
+int isEmpty(Queue* queue)
+{
+    return (queue->size == 0);
+}
+
+void enqueue(Queue* queue, int item)
+{
+    if (isFull(queue))
+        return;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size++;
+
+    cout << item << " enqueued to queue\n";
+}
+
+int dequeue(Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    return item;
+}
+
+int front(Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->front];
+}
+
+int rear(Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->rear];
+}
